d87ac592e1a89e6a4378ae45993320dfc4a18bec
ivanmmurciaua <imm117@alu.ua.es>
2021-10-13 12:42:57 +0200

    More art added

--- frontend/public/images/arte_gen.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SET output = %USERPROFILE%\Desktop\Art
-FOR /L %%G IN (1,1,50) DO (
-    timeout 2 > nul
-    curl https://thisartworkdoesnotexist.com/ > %USERPROFILE%\Desktop\Art\%%G.jpg
-)
